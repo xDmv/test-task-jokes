054837f8f03f79d47e40a8bbf2ad7c109a7238b9
Daemon <makushin.dima@gmail.com>
2020-09-02 00:17:59 +0300

    add: interfaces, note joke

---
+++ src/app/interfaces/joke.ts
@@ -0,0 +1,9 @@
+export interface Joke {
+    id: number;
+    joke: string;
+    categories: [string];
+}
+
+/*
+{ "type": "success", "value": { "id": 100, "joke": "Chuck Norris grinds his coffee with his teeth and boils the water with his own rage.", "categories": [] } }
+*/
---
+++ src/app/interfaces/note-joke.ts
@@ -0,0 +1,6 @@
+import { Joke } from './joke'
+
+export interface NoteJoke {
+    type: string;
+    value: Joke;
+}

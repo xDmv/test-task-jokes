fc129a511d97dffcc244339d83718df8d386bbb7
Daemon <makushin.dima@gmail.com>
2020-09-01 14:11:52 +0300

    add: routing pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListJokesComponent } from './pages/list-jokes/list-jokes.component';
+import { DetailJokeComponent } from './pages/detail-joke/detail-joke.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListJokesComponent},
+  { path: 'joke/:id', component: DetailJokeComponent},
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

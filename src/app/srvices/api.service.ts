c4729185b5546169dd5041c636fbc5a07aa7baaa
Daemon <makushin.dima@gmail.com>
2020-09-01 14:12:49 +0300

    add: api service, get all or one item

---
+++ src/app/srvices/api.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { tap, catchError } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
+
+const URL_API = environment.api_url;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) { }
+
+  getAlljakes(){
+    const url = `${URL_API}random/1000`
+    const result = this.http.get(url).pipe(
+			tap(res => { }),
+			catchError(e => {
+				throw e;
+			})
+		);
+		return result;
+  }
+
+  getOnejake(id: string){
+    const url = `${URL_API}${id}`
+    const result = this.http.get(url).pipe(
+			tap(res => { }),
+			catchError(e => {
+				throw e;
+			})
+		);
+		return result;
+  }
+
+}

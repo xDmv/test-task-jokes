97f1b860fc6384090fbfcdf9b1d2f91015214132
Daemon <makushin.dima@gmail.com>
2020-09-02 13:48:01 +0300

    up: APIService

--- src/app/srvices/api.service.ts
+++ src/app/srvices/api.service.ts
@@ -6,32 +6,32 @@ import { environment } from '../../environments/environment';
 const URL_API = environment.api_url;
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class ApiService {
 
-  constructor(private http: HttpClient) { }
+	constructor(private http: HttpClient) { }
 
-  getAlljakes(){
-    const url = `${URL_API}random/1000`
-    const result = this.http.get(url).pipe(
-			tap(res => { }),
-			catchError(e => {
-				throw e;
-			})
-		);
-		return result;
-  }
+	getAlljakes(){
+		const url = `${URL_API}random/1000`
+		const result = this.http.get(url).pipe(
+				tap(res => { }),
+				catchError(e => {
+					throw e;
+				})
+			);
+			return result;
+	}
 
-  getOnejake(id: string){
-    const url = `${URL_API}${id}`
-    const result = this.http.get(url).pipe(
-			tap(res => { }),
-			catchError(e => {
-				throw e;
-			})
-		);
-		return result;
-  }
+	getOnejake(id: string){
+		const url = `${URL_API}${id}`
+		const result = this.http.get(url).pipe(
+				tap(res => { }),
+				catchError(e => {
+					throw e;
+				})
+			);
+			return result;
+	}
 
 }

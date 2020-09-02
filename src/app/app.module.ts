723f42396694394df1b55303ab85ce484ea841a4
Daemon <makushin.dima@gmail.com>
2020-09-02 18:13:08 +0300

    up: app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListJokesComponent } from './pages/list-jokes/list-jokes.component';
+import { DetailJokeComponent } from './pages/detail-joke/detail-joke.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ListPaginationComponent } from './pages/list-jokes/component/list-pagination/list-pagination.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListJokesComponent,
+    DetailJokeComponent,
+    NotFoundComponent,
+    ListPaginationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

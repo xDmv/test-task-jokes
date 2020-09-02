0ca77704af9c4441d2609592726f8dd33198ca46
Daemon <makushin.dima@gmail.com>
2020-09-02 10:00:26 +0300

    add: not found page

---
+++ src/app/pages/not-found/not-found.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-not-found',
+  templateUrl: './not-found.component.html',
+  styleUrls: ['./not-found.component.scss']
+})
+export class NotFoundComponent implements OnInit {
+
+  isDark: boolean;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.themeColor(localStorage.getItem('ThemeColor'))
+  }
+
+  themeColor(val: string){
+		if(val === 'dark'){
+			this.isDark = true;
+			localStorage.setItem('ThemeColor', 'dark')
+			return
+		}
+		localStorage.setItem('ThemeColor', 'ligth')
+		this.isDark = false;
+	}
+}

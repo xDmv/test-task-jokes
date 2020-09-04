73c2d73e1f2fe6e64fad1db4c92106c005d2ee9e
Daemon <makushin.dima@gmail.com>
2020-09-04 10:10:19 +0300

    up: routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,15 @@ import { ListJokesComponent } from './pages/list-jokes/list-jokes.component';
 import { DetailJokeComponent } from './pages/detail-joke/detail-joke.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-
 const routes: Routes = [
-  { path: '', component: ListJokesComponent},
-  { path: 'joke/:id', component: DetailJokeComponent},
-  { path: '**', component: NotFoundComponent }
+  { path: '', component: ListJokesComponent },
+  { path: 'joke/:id', component: DetailJokeComponent },
+  { path: '404', component: NotFoundComponent },
+  { path: '**', redirectTo: '404' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

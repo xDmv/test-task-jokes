2effc806040758e8243dde42b1d4c3ff4796561d
Daemon <makushin.dima@gmail.com>
2020-09-04 11:54:30 +0300

    up: list jokes page

--- src/app/pages/list-jokes/list-jokes.component.ts
+++ src/app/pages/list-jokes/list-jokes.component.ts
@@ -1,35 +1,140 @@
-import { Component, OnInit } from '@angular/core';
-import { ApiService } from 'src/app/services/api.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime, map, takeUntil } from 'rxjs/operators';
+import { Joke, JokeList } from 'src/app/interfaces/note-joke';
+import { ApiService } from 'src/app/services/api.service';
+import { StateService } from 'src/app/services/state.service';
 import { NoteJoke } from '../../interfaces/note-joke';
-import { map } from 'rxjs/operators';
-import { JokeList, Joke } from 'src/app/interfaces/note-joke';
 
 @Component({
   selector: 'app-list-jokes',
   templateUrl: './list-jokes.component.html',
   styleUrls: ['./list-jokes.component.scss'],
 })
-export class ListJokesComponent implements OnInit {
+export class ListJokesComponent implements OnInit, OnDestroy {
   isDark: boolean;
-  searchText: string;
   currentPage: number;
   totalPages: number;
   totalJokes: number;
-  startJoke: number;
-  endJoke: number;
-  fullJoke = [];
-  currentArray = [];
-  listJokes = [];
+  rangeJokes: string;
+  fullJoke: Joke[];
+  currentArray: Joke[];
+  listJokes: Joke[]; 
+  searchForm: FormGroup;
+  warning: boolean;
+  notFound: boolean;
+  private ngUnsubscribe: Subject<void>;
 
-  constructor(private api: ApiService, public router: Router) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private api: ApiService,
+    private store: StateService,
+    public router: Router
+  ) {
+    this.ngUnsubscribe = new Subject();
+  }
 
   ngOnInit(): void {
-    this.themeColor(localStorage.getItem('ThemeColor'));
+    this.config();
     this.getData();
+    this.buildForms();
+  }
+
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.next();
+  }
+
+  toggleTheme(value: boolean) {
+    // @ts-ignore
+    this.store.theme = value;
+  }
+
+  config() {
+    this.store.onInit();
+
+    this.store.theme
+      .pipe(
+        // @ts-ignore
+        takeUntil(this.ngUnsubscribe)
+      )
+      .subscribe((value: boolean) => {
+        this.isDark = value;
+      });
+
+    this.store.totalPages
+      .pipe(
+        // @ts-ignore
+        takeUntil(this.ngUnsubscribe)
+      )
+      .subscribe((value: number) => {
+        this.totalPages = value;
+      });
+
+    this.store.currentPage
+      .pipe(
+        // @ts-ignore
+        takeUntil(this.ngUnsubscribe)
+      )
+      .subscribe((value: number) => {
+        this.currentPage = value;
+      });
+
+      this.store.totalJokes
+      .pipe(
+        // @ts-ignore
+        takeUntil(this.ngUnsubscribe)
+      )
+      .subscribe((value: number) => {
+        this.totalJokes = value;
+      });
+
+      this.store.rangeJokes
+      .pipe(
+        // @ts-ignore
+        takeUntil(this.ngUnsubscribe)
+      )
+      .subscribe((value: string) => {
+        this.rangeJokes = value;
+      });
+
+      this.store.currentPage
+      .pipe(
+        // @ts-ignore
+        takeUntil(this.ngUnsubscribe)
+      )
+      .subscribe((value: number) => {
+        this.currentPage = value;
+      });
+
+      this.store.shownJokes
+      .pipe(
+        // @ts-ignore
+        takeUntil(this.ngUnsubscribe)
+      )
+      .subscribe((value: Joke[]) => {
+        this.listJokes = value;
+      });
+  }
+
+  buildForms() {
+    this.searchForm = this.formBuilder.group({
+      searchString: ['', [Validators.required, Validators.minLength(2)]],
+    });
   }
 
   getData() {
+    const apiData$ = this.api.getAllJokes().pipe(
+      map((jokes: NoteJoke) => {
+        return jokes.value.map((item: JokeList) => {
+          return {
+            id: item.id,
+            text: item.joke,
+          };
+        });
+      })
+    );
     this.api
       .getAllJokes()
       .pipe(
@@ -46,6 +151,10 @@ export class ListJokesComponent implements OnInit {
         (data) => {
           this.fullJoke = data as Joke[];
           this.currentArray = this.fullJoke;
+          // @ts-ignore
+          this.store.allJokes = this.fullJoke;
+          // @ts-ignore
+          this.store.AfterSearchArray = this.fullJoke;
           this.onPagination();
         },
         (error) => {
@@ -54,55 +163,54 @@ export class ListJokesComponent implements OnInit {
       );
   }
 
-  themeColor(val: string) {
-    if (val === 'dark') {
-      this.isDark = true;
-      localStorage.setItem('ThemeColor', 'dark');
-      return;
+  startLiveSearch() {
+    if (this.searchForm.valid) {
+      this.searchForm.valueChanges.pipe(debounceTime(1500)).subscribe(() => {
+        this.warning = false;
+        this.currentArray = [];
+        const reg = new RegExp(this.searchForm.value.searchString, 'i');
+        this.fullJoke.map((item) => {
+          if (item.text.match(reg)) {
+            this.currentArray.push(item);
+          }
+        });
+        if(this.currentArray.length > 0){
+          // @ts-ignore
+          this.store.AfterSearchArray = this.currentArray;
+          this.onPagination();
+          return this.notFound = false;
+        }
+        this.notFound = true;
+      });
+    } else {
+      this.warning = true;
     }
-    localStorage.setItem('ThemeColor', 'light');
-    this.isDark = false;
-  }
-
-  onSearch() {
-    this.currentArray = [];
-    const reg = new RegExp(this.searchText, 'i');
-    this.fullJoke.map((item) => {
-      if (item.text.match(reg)) {
-        this.currentArray.push(item);
-      }
-    });
-    this.onPagination();
   }
 
   onPagination() {
-    this.listJokes = [];
-    this.totalJokes = this.currentArray.length;
-    this.startJoke = 1;
-    this.endJoke = 10;
-    this.currentPage = 1;
-    this.totalPages = Math.ceil(this.currentArray.length / 10);
-    this.listJokes = this.currentArray.slice(0, 10);
-  }
-
-  onPrev() {
-    this.startJoke = (this.currentPage - 2) * 10;
-    this.endJoke = (this.currentPage - 1) * 10;
-    this.currentPage = this.currentPage - 1;
-    this.listJokes = this.currentArray.slice(this.startJoke, this.endJoke);
-  }
-
-  onNext() {
-    this.startJoke = this.currentPage * 10;
-    this.endJoke = (this.currentPage + 1) * 10;
-    this.currentPage = this.currentPage + 1;
-    this.listJokes = this.currentArray.slice(this.startJoke, this.endJoke);
-  }
-
-  onPage(index: number) {
-    this.startJoke = (index - 1) * 10;
-    this.endJoke = index * 10;
-    this.currentPage = index;
-    this.listJokes = this.currentArray.slice(this.startJoke, this.endJoke);
+    this.store.AfterSearchArray
+    .pipe(
+      // @ts-ignore
+      takeUntil(this.ngUnsubscribe)
+    )
+    .subscribe((value: Joke[]) => {
+      this.currentArray = value;
+      let endJoke = 0;
+      if(this.currentArray.length  > 10){
+        endJoke = 10;
+      } else {
+        endJoke = this.currentArray.length;
+      }
+      // @ts-ignore
+      this.store.totalJokes = this.currentArray.length;
+      // @ts-ignore
+      this.store.totalPages = this.currentArray.length > 10 ? Math.ceil(this.currentArray.length / 10) : 1;
+      // @ts-ignore
+      this.store.rangeJokes = `1-${endJoke}`;
+      // @ts-ignore
+      this.store.currentPage = 1;
+      // @ts-ignore
+      this.store.shownJokes = this.currentArray.slice(0, 10);
+    });
   }
 }

8906eb60250ce8bd92291d5255696926d37ecc27
Daemon <makushin.dima@gmail.com>
2020-09-04 11:47:56 +0300

    up: not found component

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,26 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { StateService } from '../../services/state.service';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.scss'],
 })
-export class NotFoundComponent implements OnInit {
+export class NotFoundComponent implements OnInit, OnDestroy {
   isDark: boolean;
+  private ngUnsubscribe: Subject<void>;
 
-  constructor() {}
-
-  ngOnInit(): void {
-    this.themeColor(localStorage.getItem('ThemeColor'));
+  constructor(private store: StateService) {
+    this.ngUnsubscribe = new Subject();
   }
 
-  themeColor(val: string) {
-    if (val === 'dark') {
-      this.isDark = true;
-      localStorage.setItem('ThemeColor', 'dark');
-      return;
-    }
-    localStorage.setItem('ThemeColor', 'light');
-    this.isDark = false;
+  ngOnInit(): void {
+    this.store.theme
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe((value: boolean) => {
+        this.isDark = value;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.next();
   }
 }
